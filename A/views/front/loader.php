807292a8dfbab5b47795e82e9992fa0f51ab1fc1
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-20 18:09:24 +0530

    update

--- A/views/front/loader.php
+++
@@ -1,6 +0,0 @@
-<div id="preloader">
-    <div class="canvas">
-        <img src="<?php echo base_url().'assets/images/logo.png';?>" alt="logo" class="loader-logo">
-        <div class="spinner"></div>   
-    </div>
-</div>
